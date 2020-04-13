5ed4efaa3b511de39cf7319bfae88536c5af1cef
ahmedbhameed <ahmedbazy@gmail.com>
2020-04-13 14:35:44 +0200

    fix linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
+        './node_modules/gts',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -12,5 +13,9 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'node/no-unsupported-features/node-builtins': ['error', { version: '>=10.0.0', ignores: [] }],
     },
+    env: { jest: true },
 };
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Request, Response } from 'express';
-import { resolve } from 'path';
+import express, {Request, Response} from 'express';
+import {resolve} from 'path';
 
 const PORT = 5000;
 const app = express();
 
 app.use(express.static(resolve(__dirname, 'public')));
 
-app.get('/', (req: any, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.set('Content-Type', 'text/html').sendFile(resolve('./public/index.html'));
 });
 
-var foo = someFunction();
 app.listen(PORT, function () {
     console.log(`App is running at http://localhost:${PORT}`);
 });
